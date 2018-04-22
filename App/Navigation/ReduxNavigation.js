95765579048cd4b00bccb06eacfbb57e317dd97f
Okanlawon Emmanunel <okanlawonemmanuel41.com>
2018-04-22 12:12:16 +0100

    init

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -3,12 +3,27 @@ import * as ReactNavigation from 'react-navigation'
 import { connect } from 'react-redux'
 import AppNavigation from './AppNavigation'
 
+// for react-navigation 1.0.0-beta.30
+import {
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers';
+
+const middleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav,
+);
+const addListener = createReduxBoundAddListener("root");
+// end for react-navigation 1.0.0-beta.30
+
+
 // here is our redux-aware smart component
-function ReduxNavigation (props) {
+function ReduxNavigation(props) {
   const { dispatch, nav } = props
   const navigation = ReactNavigation.addNavigationHelpers({
     dispatch,
-    state: nav
+    state: nav,
+    addListener
   })
 
   return <AppNavigation navigation={navigation} />
